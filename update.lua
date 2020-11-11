372cf903f73fbd460c511c620674e19c1bd700a1
rk00 <ruslan.kovtun@gmail.com>
2020-11-11 14:24:24 +0200

    Update update.lua
    change token to permanent

--- update.lua
+++ update.lua
@@ -1,3 +1,3 @@
 localPath = scriptPath()
 imagePath = (localPath .. "image/")
-httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/update.lua?token=ADBAEXHAO2PGFYA7QB7WX6C7VPK7W", localPath .. "main.lua")
+httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/update.lua?token=b983b7a243cc3d5661e50e2674f27cb61d7dd5f6", localPath .. "main.lua")
