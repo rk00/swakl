0ae0922405e58d19d0b87268a3ed26b063cde4c3
rk00 <ruslan.kovtun@gmail.com>
2020-11-11 15:58:06 +0200

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,5 +1,4 @@
 localPath = scriptPath()
 imagePath = (localPath .. "image/")
---httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/update.lua?token=ADBAEXFWR2GZF57PIME5FPC7VPMGI", localPath .. "main.lua")
-checkVersion = os.execute("curl -H 'Authorization: token b983b7a243cc3d5661e50e2674f27cb61d7dd5f6' https://raw.githubusercontent.com/rk00/swakl/main/version.txt")
-print(checkVersion)
+latestVersion = loadstring(httpGet("https://raw.githubusercontent.com/rk00/swakl/main/version.txt"))
+print(latestVersion)
