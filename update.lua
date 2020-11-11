04070c677032648ee65584850bec57861393b845
rk00 <ruslan.kovtun@gmail.com>
2020-11-11 14:25:46 +0200

    Update update.lua
    test new token

--- update.lua
+++ update.lua
@@ -1,3 +1,3 @@
 localPath = scriptPath()
 imagePath = (localPath .. "image/")
-httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/update.lua?token=b983b7a243cc3d5661e50e2674f27cb61d7dd5f6", localPath .. "main.lua")
+httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/update.lua?token=ADBAEXFWR2GZF57PIME5FPC7VPMGI", localPath .. "main.lua")
