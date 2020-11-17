e45a8217e27990c141df8e090b40babc3aeda1da
rk00 <ruslan.kovtun@gmail.com>
2020-11-17 11:59:59 +0200

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,17 +1,21 @@
 localPath = scriptPath()
 imagePath = (localPath .. "image/")
+
 newestVersion = loadstring(httpGet("https://raw.githubusercontent.com/rk00/swakl/main/version.lua"))()
-currentVersion = dofile(localPath .. "version.lua")
+
+if(io.open(localPath .. "version.lua","r")) then
+	currentVersion = dofile(localPath .. "version.lua")
+else
+	currentVersion = "0.0.0"
+end
 print(newestVersion)
 print(currentVersion)
 if currentVersion == newestVersion then
   toast("Running latest version")
 else
   httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/version.lua", localPath .."version.lua")
-  httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/update.lua", localPath .."update.lua")
+  httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/main.lua", localPath .."main.lua")
   scriptExit("You have updated script!")
 end
 
-while true do
-  wait(5)
-end
+dofile(localPath .. "version.lua")
