1e5befef7a25c4ebe07621fba43a518104befd0b
rk00 <ruslan.kovtun@gmail.com>
2020-11-17 12:40:20 +0200

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,5 +1,4 @@
 localPath = scriptPath()
-imagePath = (localPath .. "image/")
 
 newestVersion = loadstring(httpGet("https://raw.githubusercontent.com/rk00/swakl/main/version.lua"))()
 
@@ -8,9 +7,10 @@ if(io.open(localPath .. "version.lua","r")) then
 else
 	currentVersion = "v0.0.0"
 end
+
 if currentVersion ~= newestVersion then
   httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/version.lua", localPath .."version.lua")
   httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/main.lua", localPath .."main.lua")
 end
 
-dofile(localPath .. "version.lua")
+dofile(localPath .. "main.lua")
