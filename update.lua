9119c2ca1f0fce84b1fe0fb8994adeca476c7a6f
rk00 <ruslan.kovtun@gmail.com>
2020-11-17 12:03:36 +0200

    Update update.lua
    clean debug

--- update.lua
+++ update.lua
@@ -6,16 +6,11 @@ newestVersion = loadstring(httpGet("https://raw.githubusercontent.com/rk00/swakl
 if(io.open(localPath .. "version.lua","r")) then
 	currentVersion = dofile(localPath .. "version.lua")
 else
-	currentVersion = "0.0.0"
+	currentVersion = "v0.0.0"
 end
-print(newestVersion)
-print(currentVersion)
-if currentVersion == newestVersion then
-  toast("Running latest version")
-else
+if currentVersion ~= newestVersion then
   httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/version.lua", localPath .."version.lua")
   httpDownload("https://raw.githubusercontent.com/rk00/swakl/main/main.lua", localPath .."main.lua")
-  scriptExit("You have updated script!")
 end
 
 dofile(localPath .. "version.lua")
